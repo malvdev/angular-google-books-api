cfedd0b12ebe550521e3ac87d2bece2783a572ee
malvdev <sergey.malkov.dev@gmail.com>
2021-07-22 16:17:15 +0500

    feat(book): add book entity

---
+++ libs/book/domain/src/lib/entities/book.entity.ts
@@ -0,0 +1,55 @@
+export type UniqueId = string;
+export type URL = string;
+export type Language = string;
+export type DateTimeString = string;
+
+export interface Price {
+  amount: number;
+  currencyCode: string;
+}
+
+export interface IndustryIdentifier {
+  type: string;
+  identifier: UniqueId;
+}
+
+export interface BookEntity {
+  id: UniqueId;
+  etag: string;
+  selfLink: URL;
+  volumeInfo: {
+    title: string;
+    authors: string[];
+    publisher: string;
+    publishDate: DateTimeString;
+    printType: string;
+    pageCount: number;
+    description: string;
+    averageRating: number;
+    ratingsCount: number;
+    categories: string[];
+    language: string;
+    previewLink: URL;
+    infoLink: URL;
+    imageLinks: {
+      thumbnail: URL;
+      smallThumbnail: URL;
+      medium: URL;
+      large: URL;
+      extraLarge: URL;
+    };
+    canonicalVolumeLink: URL;
+    industryIdentifiers: IndustryIdentifier[];
+  };
+  saleInfo: {
+    country: string;
+    saleability: string;
+    coisEbookuntry: boolean;
+    listPrice: Price;
+    retailPrice: Price;
+    buyLink: URL;
+  };
+  searchInfo: {
+    textSnippet: string;
+  };
+}
