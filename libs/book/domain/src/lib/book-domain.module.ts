fd826948c922d771eaa756ca368d9302438cfc48
malvdev <sergey.malkov.dev@gmail.com>
2021-07-22 17:02:11 +0500

    feat(book): add book service
    The book service for interacting with the Google Book API

--- libs/book/domain/src/lib/book-domain.module.ts
+++ libs/book/domain/src/lib/book-domain.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { BookService } from './infrastructure';
+
 @NgModule({
   imports: [CommonModule],
+  providers: [BookService],
 })
 export class BookDomainModule {}
