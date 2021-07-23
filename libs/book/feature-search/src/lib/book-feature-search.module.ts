11c949d774e362a8cc6bf9af35a073f9cdf13f64
malvdev <sergey.malkov.dev@gmail.com>
2021-07-23 12:02:35 +0500

    feat(book-search): add routing for search module

--- libs/book/feature-search/src/lib/book-feature-search.module.ts
+++ libs/book/feature-search/src/lib/book-feature-search.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { BookSearchComponent } from './book-search.component';
+import { BookFeatureSearchRoutingModule } from './book-feature-search-routing.module';
+
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, BookFeatureSearchRoutingModule],
+  declarations: [BookSearchComponent],
 })
 export class BookFeatureSearchModule {}
