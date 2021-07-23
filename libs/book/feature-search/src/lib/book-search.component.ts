6d8a47c5828c637216c7b4babf8e69b5677f0be1
malvdev <sergey.malkov.dev@gmail.com>
2021-07-23 12:01:33 +0500

    feat(book-search): add book search component

---
+++ libs/book/feature-search/src/lib/book-search.component.ts
@@ -0,0 +1,9 @@
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({
+  selector: 'app-book-search',
+  templateUrl: './book-search.component.html',
+  styleUrls: ['./book-search.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class BookSearchComponent {}
