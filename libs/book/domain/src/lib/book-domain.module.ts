186d184552879168d3e24176d3ea1661f4bf6c48
malvdev <sergey.malkov.dev@gmail.com>
2021-07-23 09:58:03 +0500

    feat(book): add book facade

--- libs/book/domain/src/lib/book-domain.module.ts
+++ libs/book/domain/src/lib/book-domain.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { BookService } from './infrastructure';
+import { BookFacade } from './application';
 
 @NgModule({
   imports: [CommonModule],
-  providers: [BookService],
+  providers: [BookService, BookFacade],
 })
 export class BookDomainModule {}
