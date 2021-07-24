045ec28ab87f2419bc572e3758ac2129434a345b
malvdev <sergey.malkov.dev@gmail.com>
2021-07-24 09:30:55 +0500

    fix(shared): change prefix for component and directive

--- libs/shared/ui/src/lib/main-template/main-template.component.ts
+++ libs/shared/ui/src/lib/main-template/main-template.component.ts
@@ -1,7 +1,7 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
-  selector: 'app-shared-main-template',
+  selector: 'app-shared-ui-main-template',
   templateUrl: './main-template.component.html',
   styleUrls: ['./main-template.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
