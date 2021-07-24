dd59b273a046b2d37313d85181f2836b2232ca27
malvdev <sergey.malkov.dev@gmail.com>
2021-07-24 11:16:51 +0500

    feat(shared): add pipe truncate

--- libs/shared/pipes/src/index.ts
+++ libs/shared/pipes/src/index.ts
@@ -1,1 +1,2 @@
 export * from './lib/shared-pipes.module';
+export * from './lib/truncate';
--- libs/shared/pipes/src/lib/shared-pipes.module.ts
+++ libs/shared/pipes/src/lib/shared-pipes.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { TruncateModule } from './truncate/';
+
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, TruncateModule],
 })
 export class SharedPipesModule {}
