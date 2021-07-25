95b81e3eb9da8c3acb7947b2476c946a0eea2580
malvdev <sergey.malkov.dev@gmail.com>
2021-07-25 17:14:33 +0500

    feat(book-search): add book search component

--- libs/book/feature-search/src/lib/book-search.component.spec.ts
+++ libs/book/feature-search/src/lib/book-search.component.spec.ts
@@ -1,6 +1,19 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
+
+import { MainTemplateModule } from '@libs/google-books-api/shared/ui';
+import {
+  BookItemListModule,
+  SearchFormModule,
+} from '@libs/google-books-api/book/ui';
 
 import { BookSearchComponent } from './book-search.component';
+import { BookService } from '@libs/google-books-api/book/domain';
 
 describe('BookSearchComponent', () => {
   let component: BookSearchComponent;
@@ -9,6 +22,24 @@ describe('BookSearchComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [BookSearchComponent],
+      imports: [
+        NoopAnimationsModule,
+        HttpClientTestingModule,
+        RouterTestingModule,
+        MatPaginatorModule,
+        SearchFormModule,
+        MainTemplateModule,
+        BookItemListModule,
+      ],
+      providers: [
+        BookService,
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            queryParams: of({ q: 'typescript' }),
+          },
+        },
+      ],
     }).compileComponents();
   });
 
