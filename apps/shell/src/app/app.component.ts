b46f3e17d4f437f1c50d9baa4f0cab83853c7114
malvdev <sergey.malkov.dev@gmail.com>
2021-07-28 22:06:22 +0500

    refactor(app): rename prefix for app shell to app-shell

--- apps/shell/src/app/app.component.ts
+++ apps/shell/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'angular-google-books-api-root',
+  selector: 'app-shell-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
