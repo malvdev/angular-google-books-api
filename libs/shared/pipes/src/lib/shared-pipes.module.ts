94299aca3a75e2623bd09418bf2d27dc6f6eff15
malvdev <sergey.malkov.dev@gmail.com>
2021-07-29 08:43:19 +0500

    fix(shared-pipes): remove slash from import

--- libs/shared/pipes/src/lib/shared-pipes.module.ts
+++ libs/shared/pipes/src/lib/shared-pipes.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { TruncateModule } from './truncate/';
+import { TruncateModule } from './truncate';
 
 @NgModule({
   imports: [CommonModule, TruncateModule],
