1a828403da426189385f712598ca79bba20e5f4b
malvdev <sergey.malkov.dev@gmail.com>
2021-07-29 12:35:41 +0500

    refactor(book-ui): remove wrapper link

--- libs/book/ui/src/lib/header/header.component.spec.ts
+++ libs/book/ui/src/lib/header/header.component.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
 
 import { HeaderComponent } from './header.component';
 
@@ -10,7 +9,6 @@ describe('HeaderComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [HeaderComponent],
-      imports: [RouterTestingModule],
     }).compileComponents();
   });
 
--- libs/book/ui/src/lib/header/header.module.ts
+++ libs/book/ui/src/lib/header/header.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
 
 import { HeaderComponent } from './header.component';
 
 @NgModule({
   declarations: [HeaderComponent],
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule],
   exports: [HeaderComponent],
 })
 export class HeaderModule {}
